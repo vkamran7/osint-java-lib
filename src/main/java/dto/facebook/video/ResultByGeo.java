f835b939a06cb6620c59e794370146e22df8a792
kamran <kamran.valizada@mdm.gov.az>
2021-02-11 09:30:41 +0400

    Added DTO for Facebook video response by geo

---
+++ src/main/java/dto/facebook/video/ResultByGeo.java
@@ -0,0 +1,13 @@
+package dto.facebook.video;
+
+public class ResultByGeo extends Result {
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ src/main/java/dto/facebook/video/VideoByGeoResponse.java
@@ -0,0 +1,5 @@
+package dto.facebook.video;
+
+public class VideoByGeoResponse {
+
+}
