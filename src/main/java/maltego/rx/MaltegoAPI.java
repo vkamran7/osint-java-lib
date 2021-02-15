83ff8b57de9658ad5ebc071898a81f6a35f67604
kamran <kamran.valizada@mdm.gov.az>
2021-02-15 16:56:16 +0400

    added request dto FacebookVideoV2Request class

--- src/main/java/maltego/rx/MaltegoAPI.java
+++ src/main/java/maltego/rx/MaltegoAPI.java
@@ -1,5 +1,6 @@
 package maltego.rx;
 
+import model.facebook.video.FacebookVideoByGeoResponse;
 import model.facebook.video.FacebookVideoV2Response;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -10,5 +11,8 @@ public interface MaltegoAPI {
     @GET("facebook/search_videos/v2")
     Observable<FacebookVideoV2Response> getFacebookVideoV2(@Query("query") String query,
                                                            @Query("limit") int limit);
+
+    @GET("facebook/graph_query_by_geo")
+    Observable<FacebookVideoByGeoResponse> getFacebookVideoGeo();
 
 }
---
+++ src/main/java/model/facebook/video/FacebookVideoV2Request.java
@@ -0,0 +1,81 @@
+package model.facebook.video;
+
+public class FacebookVideoV2Request {
+    private String query;
+    private Integer limit;
+    private String location;
+    private String date;
+    private String timeout;
+
+    public static class Builder {
+
+        private String query;
+        private Integer limit;
+        private String location;
+        private String date;
+        private String timeout;
+
+        public Builder() {
+        }
+
+        Builder(String query, Integer limit, String location, String date, String timeout) {
+            this.query = query;
+            this.limit = limit;
+            this.location = location;
+            this.date = date;
+            this.timeout = timeout;
+        }
+
+        public Builder query(String query){
+            this.query = query;
+            return Builder.this;
+        }
+
+        public Builder limit(Integer limit){
+            this.limit = limit;
+            return Builder.this;
+        }
+
+        public Builder location(String location){
+            this.location = location;
+            return Builder.this;
+        }
+
+        public Builder date(String date){
+            this.date = date;
+            return Builder.this;
+        }
+
+        public Builder timeout(String timeout){
+            this.timeout = timeout;
+            return Builder.this;
+        }
+
+        public FacebookVideoV2Request build() {
+            if(this.query == null){
+                throw new NullPointerException("The property \"query\" is null. "
+                        + "Please set the value by \"query()\". "
+                        + "The properties \"query\", \"limit\" are required.");
+            }
+            if(this.limit == null){
+                throw new NullPointerException("The property \"limit\" is null. "
+                        + "Please set the value by \"limit()\". "
+                        + "The properties \"query\", \"limit\" are required.");
+            }
+
+            return new FacebookVideoV2Request(this);
+        }
+    }
+
+    private FacebookVideoV2Request(Builder builder) {
+        this.query = builder.query;
+        this.limit = builder.limit;
+        this.location = builder.location;
+        this.date = builder.date;
+        this.timeout = builder.timeout;
+    }
+
+    public void doSomething() {
+        // do something
+    }
+}
