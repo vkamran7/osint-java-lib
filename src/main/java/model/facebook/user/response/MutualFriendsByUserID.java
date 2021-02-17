cb2d252de24cd0ac16bf44bf7905dabebda89725
kamran <kamran.valizada@mdm.gov.az>
2021-02-17 16:41:08 +0400

    added MutualFriendsByUserID dto class for facebook user in maltego api

---
+++ src/main/java/model/facebook/user/response/MutualFriendsByUserID.java
@@ -0,0 +1,62 @@
+package model.facebook.user.response;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class MutualFriendsByUserID {
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("profile-image")
+    @Expose
+    private String profileImage;
+    @SerializedName("surl")
+    @Expose
+    private String surl;
+    @SerializedName("alias")
+    @Expose
+    private String alias;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public String getSurl() {
+        return surl;
+    }
+
+    public void setSurl(String surl) {
+        this.surl = surl;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+}
