a9e81620328a6e17eae129c95851420cba24ea6e
Kamran <vkamran7@gmail.com>
2021-02-21 16:30:56 +0400

    added PagesByNameRequest dto

---
+++ src/main/java/model/facebook/page/request/PagesByNameRequest.java
@@ -0,0 +1,44 @@
+package model.facebook.page.request;
+
+public class PagesByNameRequest {
+    private String query;
+    private Integer limit;
+    private Integer timeout;
+
+
+    public static final class Builder {
+        private String query;
+        private Integer limit;
+        private Integer timeout;
+
+        public Builder() {
+        }
+
+        public static Builder aPagesByNameRequest() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public Builder withTimeout(Integer timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public PagesByNameRequest build() {
+            PagesByNameRequest pagesByNameRequest = new PagesByNameRequest();
+            pagesByNameRequest.query = this.query;
+            pagesByNameRequest.timeout = this.timeout;
+            pagesByNameRequest.limit = this.limit;
+            return pagesByNameRequest;
+        }
+    }
+}
