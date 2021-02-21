409c8a79575f9bb7b20ecb7938424977bb05c83f
Kamran <vkamran7@gmail.com>
2021-02-21 15:46:47 +0400

    added AlbumsByPageRequest dto to page package

---
+++ src/main/java/model/facebook/page/request/AlbumsByPageRequest.java
@@ -0,0 +1,44 @@
+package model.facebook.page.request;
+
+public class AlbumsByPageRequest {
+    private String query; // Facebook Page ID
+    private Integer limit;
+    private Integer timeout;
+
+
+    public static final class Builder {
+        private String query; // Facebook Page ID
+        private Integer limit;
+        private Integer timeout;
+
+        public Builder() {
+        }
+
+        public static Builder anAlbumsByPageRequest() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public Builder withTimeout(Integer timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public AlbumsByPageRequest build() {
+            AlbumsByPageRequest albumsByPageRequest = new AlbumsByPageRequest();
+            albumsByPageRequest.limit = this.limit;
+            albumsByPageRequest.query = this.query;
+            albumsByPageRequest.timeout = this.timeout;
+            return albumsByPageRequest;
+        }
+    }
+}
