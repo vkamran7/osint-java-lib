8939c69a7b016e96cdd099f7f1e7e2a823368407
Kamran <vkamran7@gmail.com>
2021-02-21 19:28:31 +0400

    added RepostsRequest dto

---
+++ src/main/java/model/facebook/post_reaction_place/request/RepostsRequest.java
@@ -0,0 +1,44 @@
+package model.facebook.post_reaction_place.request;
+
+public class RepostsRequest {
+    private String query;
+    private Integer timeout;
+    private Integer limit;
+
+
+    public static final class Builder {
+        private String query;
+        private Integer timeout;
+        private Integer limit;
+
+        public Builder() {
+        }
+
+        public static Builder aRepostsRequest() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder withTimeout(Integer timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public RepostsRequest build() {
+            RepostsRequest repostsRequest = new RepostsRequest();
+            repostsRequest.timeout = this.timeout;
+            repostsRequest.limit = this.limit;
+            repostsRequest.query = this.query;
+            return repostsRequest;
+        }
+    }
+}
