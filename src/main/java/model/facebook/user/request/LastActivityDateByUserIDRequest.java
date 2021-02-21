552dc2f4cf635fe1e2e3616c6dd759ebacb553a1
Kamran <vkamran7@gmail.com>
2021-02-21 13:30:28 +0400

    added LastActivityDateByUserIDRequest dto to user package

---
+++ src/main/java/model/facebook/user/request/LastActivityDateByUserIDRequest.java
@@ -0,0 +1,36 @@
+package model.facebook.user.request;
+
+public class LastActivityDateByUserIDRequest {
+    private String query; // facebook user id
+    private Integer timeout;
+
+
+    public static final class Builder {
+        private String query; // facebook user id
+        private Integer timeout;
+
+        public Builder() {
+        }
+
+        public static Builder aLastActivityDateByUserIDRequest() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder withTimeout(Integer timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public LastActivityDateByUserIDRequest build() {
+            LastActivityDateByUserIDRequest lastActivityDateByUserIDRequest = new LastActivityDateByUserIDRequest();
+            lastActivityDateByUserIDRequest.timeout = this.timeout;
+            lastActivityDateByUserIDRequest.query = this.query;
+            return lastActivityDateByUserIDRequest;
+        }
+    }
+}
