d9a91200cf4c9cf68cf2510e77504f98aa4c04f5
Kamran <vkamran7@gmail.com>
2021-02-21 15:18:56 +0400

    added UserIDByAliasRequest dto to user package

---
+++ src/main/java/model/facebook/user/request/UserIDByAliasRequest.java
@@ -0,0 +1,28 @@
+package model.facebook.user.request;
+
+public class UserIDByAliasRequest {
+    private String alias; // User alias
+
+
+    public static final class Builder {
+        private String alias; // User alias
+
+        public Builder() {
+        }
+
+        public static Builder anUserIDByAliasRequest() {
+            return new Builder();
+        }
+
+        public Builder withAlias(String alias) {
+            this.alias = alias;
+            return this;
+        }
+
+        public UserIDByAliasRequest build() {
+            UserIDByAliasRequest userIDByAliasRequest = new UserIDByAliasRequest();
+            userIDByAliasRequest.alias = this.alias;
+            return userIDByAliasRequest;
+        }
+    }
+}
