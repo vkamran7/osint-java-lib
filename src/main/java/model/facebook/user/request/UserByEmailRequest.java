d7bbbb1ac7233c73af2a0d6f66a9bde2f629e226
Kamran <vkamran7@gmail.com>
2021-02-21 14:02:49 +0400

    added UserByEmailRequest dto to user package

---
+++ src/main/java/model/facebook/user/request/UserByEmailRequest.java
@@ -0,0 +1,28 @@
+package model.facebook.user.request;
+
+public class UserByEmailRequest {
+    private String query; // email
+
+
+    public static final class Builder {
+        private String query; // email
+
+        public Builder() {
+        }
+
+        public static Builder anUserByEmailRequest() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public UserByEmailRequest build() {
+            UserByEmailRequest userByEmailRequest = new UserByEmailRequest();
+            userByEmailRequest.query = this.query;
+            return userByEmailRequest;
+        }
+    }
+}
