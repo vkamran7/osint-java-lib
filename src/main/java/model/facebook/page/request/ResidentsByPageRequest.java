c065a7f73138b0404e396e760455fca89e336e76
Kamran <vkamran7@gmail.com>
2021-02-21 17:09:46 +0400

    added ResidentsByPageRequest dto

---
+++ src/main/java/model/facebook/page/request/ResidentsByPageRequest.java
@@ -0,0 +1,44 @@
+package model.facebook.page.request;
+
+public class ResidentsByPageRequest {
+    private String query; // Facebook Page ID
+    private Integer limit;
+    private Integer timeout;
+
+
+    public static final class Builder {
+        private String query; // Facebook Page ID
+        private Integer limit;
+        private Integer timeout;
+
+        public Builder() {
+        }
+
+        public static Builder aResidentsByPageRequest() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public Builder withTimeout(Integer timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public ResidentsByPageRequest build() {
+            ResidentsByPageRequest residentsByPageRequest = new ResidentsByPageRequest();
+            residentsByPageRequest.query = this.query;
+            residentsByPageRequest.limit = this.limit;
+            residentsByPageRequest.timeout = this.timeout;
+            return residentsByPageRequest;
+        }
+    }
+}
