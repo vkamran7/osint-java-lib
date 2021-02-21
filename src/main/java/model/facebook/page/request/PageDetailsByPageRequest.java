3af053f65e07d9ca5edd08058dfcfd43c732284a
Kamran <vkamran7@gmail.com>
2021-02-21 16:25:55 +0400

    added PageDetailsByPageRequest dto to page package

---
+++ src/main/java/model/facebook/page/request/PageDetailsByPageRequest.java
@@ -0,0 +1,28 @@
+package model.facebook.page.request;
+
+public class PageDetailsByPageRequest {
+    private String query; // Page ID
+
+
+    public static final class Builder {
+        private String query; // Page ID
+
+        public Builder() {
+        }
+
+        public static Builder aPageDetailsByPageRequest() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public PageDetailsByPageRequest build() {
+            PageDetailsByPageRequest pageDetailsByPageRequest = new PageDetailsByPageRequest();
+            pageDetailsByPageRequest.query = this.query;
+            return pageDetailsByPageRequest;
+        }
+    }
+}
