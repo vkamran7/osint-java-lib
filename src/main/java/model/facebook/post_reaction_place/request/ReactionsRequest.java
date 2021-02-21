a2ff1bd7fa6105c586875b1fd7ac35a944e928b1
Kamran <vkamran7@gmail.com>
2021-02-21 19:23:43 +0400

    added ReactionsRequest dto

---
+++ src/main/java/model/facebook/post_reaction_place/request/ReactionsRequest.java
@@ -0,0 +1,44 @@
+package model.facebook.post_reaction_place.request;
+
+public class ReactionsRequest {
+    private Long id;
+    private Integer limit;
+    private Integer isComment;
+
+
+    public static final class Builder {
+        private Long id;
+        private Integer limit;
+        private Integer isComment;
+
+        public Builder() {
+        }
+
+        public static Builder aReactionsRequest() {
+            return new Builder();
+        }
+
+        public Builder withId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public Builder withIsComment(Integer isComment) {
+            this.isComment = isComment;
+            return this;
+        }
+
+        public ReactionsRequest build() {
+            ReactionsRequest reactionsRequest = new ReactionsRequest();
+            reactionsRequest.isComment = this.isComment;
+            reactionsRequest.limit = this.limit;
+            reactionsRequest.id = this.id;
+            return reactionsRequest;
+        }
+    }
+}
