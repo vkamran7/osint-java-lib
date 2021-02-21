e14c63531cc22120ff824b94cbcfd3a9c78cc734
Kamran <vkamran7@gmail.com>
2021-02-21 17:00:04 +0400

    added RelatedPagesByPageRequest dto

---
+++ src/main/java/model/facebook/page/request/RelatedPagesByPageRequest.java
@@ -0,0 +1,36 @@
+package model.facebook.page.request;
+
+public class RelatedPagesByPageRequest {
+    private String query; // Facebook Page ID;
+    private Integer limit;
+
+
+    public static final class Builder {
+        private String query; // Facebook Page ID;
+        private Integer limit;
+
+        public Builder() {
+        }
+
+        public static Builder aRelatedPagesByPageRequest() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public RelatedPagesByPageRequest build() {
+            RelatedPagesByPageRequest relatedPagesByPageRequest = new RelatedPagesByPageRequest();
+            relatedPagesByPageRequest.limit = this.limit;
+            relatedPagesByPageRequest.query = this.query;
+            return relatedPagesByPageRequest;
+        }
+    }
+}
