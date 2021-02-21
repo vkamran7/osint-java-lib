6fa115157b39944949809049592e3a151ee3dc9b
Kamran <vkamran7@gmail.com>
2021-02-21 16:55:22 +0400

    added PlacesByNameRequest dto

---
+++ src/main/java/model/facebook/page/request/PlacesByNameRequest.java
@@ -0,0 +1,36 @@
+package model.facebook.page.request;
+
+public class PlacesByNameRequest {
+    private String query;
+    private Integer limit;
+
+
+    public static final class Builder {
+        private String query;
+        private Integer limit;
+
+        public Builder() {
+        }
+
+        public static Builder aPlacesByNameRequest() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public PlacesByNameRequest build() {
+            PlacesByNameRequest placesByNameRequest = new PlacesByNameRequest();
+            placesByNameRequest.query = this.query;
+            placesByNameRequest.limit = this.limit;
+            return placesByNameRequest;
+        }
+    }
+}
