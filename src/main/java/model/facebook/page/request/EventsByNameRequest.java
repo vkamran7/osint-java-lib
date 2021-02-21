02d3171254d7c5cee40078a1c31013969168e02d
Kamran <vkamran7@gmail.com>
2021-02-21 16:03:33 +0400

    added EventsByNameRequest dto

---
+++ src/main/java/model/facebook/page/request/EventsByNameRequest.java
@@ -0,0 +1,44 @@
+package model.facebook.page.request;
+
+public class EventsByNameRequest {
+    private String query;
+    private Integer limit;
+    private Integer timeout;
+
+
+    public static final class Builder {
+        private String query;
+        private Integer limit;
+        private Integer timeout;
+
+        public Builder() {
+        }
+
+        public static Builder anEventsByNameRequest() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public Builder withTimeout(Integer timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public EventsByNameRequest build() {
+            EventsByNameRequest eventsByNameRequest = new EventsByNameRequest();
+            eventsByNameRequest.limit = this.limit;
+            eventsByNameRequest.timeout = this.timeout;
+            eventsByNameRequest.query = this.query;
+            return eventsByNameRequest;
+        }
+    }
+}
