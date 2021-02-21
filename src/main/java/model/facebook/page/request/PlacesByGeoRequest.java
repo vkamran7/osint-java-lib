41915e58eb81ba4f35c9769625287ae36bf86cb3
Kamran <vkamran7@gmail.com>
2021-02-21 16:47:21 +0400

    added PlacesByGeoRequest dto

---
+++ src/main/java/model/facebook/page/request/PlacesByGeoRequest.java
@@ -0,0 +1,52 @@
+package model.facebook.page.request;
+
+public class PlacesByGeoRequest {
+    private Double lat;
+    private Double lon;
+    private Integer distance;
+    private Integer limit;
+
+
+    public static final class Builder {
+        private Double lat;
+        private Double lon;
+        private Integer distance;
+        private Integer limit;
+
+        public Builder() {
+        }
+
+        public static Builder aPlacesByGeoRequest() {
+            return new Builder();
+        }
+
+        public Builder withLat(Double lat) {
+            this.lat = lat;
+            return this;
+        }
+
+        public Builder withLon(Double lon) {
+            this.lon = lon;
+            return this;
+        }
+
+        public Builder withDistance(Integer distance) {
+            this.distance = distance;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public PlacesByGeoRequest build() {
+            PlacesByGeoRequest placesByGeoRequest = new PlacesByGeoRequest();
+            placesByGeoRequest.distance = this.distance;
+            placesByGeoRequest.limit = this.limit;
+            placesByGeoRequest.lat = this.lat;
+            placesByGeoRequest.lon = this.lon;
+            return placesByGeoRequest;
+        }
+    }
+}
