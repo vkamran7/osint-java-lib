5626f0f069a3970d815f0eaec26ab62b5ee6b078
Kamran <vkamran7@gmail.com>
2021-02-21 16:57:28 +0400

    added PlacesInByPageRequest dto to page package

---
+++ src/main/java/model/facebook/page/request/PlacesInByPageRequest.java
@@ -0,0 +1,44 @@
+package model.facebook.page.request;
+
+public class PlacesInByPageRequest {
+    private String query; // Facebook Page ID
+    private Integer limit;
+    private Integer timeout;
+
+
+    public static final class Builder {
+        private String query; // Facebook Page ID
+        private Integer limit;
+        private Integer timeout;
+
+        public Builder() {
+        }
+
+        public static Builder aPlacesInByPageRequest() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public Builder withTimeout(Integer timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public PlacesInByPageRequest build() {
+            PlacesInByPageRequest placesInByPageRequest = new PlacesInByPageRequest();
+            placesInByPageRequest.limit = this.limit;
+            placesInByPageRequest.timeout = this.timeout;
+            placesInByPageRequest.query = this.query;
+            return placesInByPageRequest;
+        }
+    }
+}
