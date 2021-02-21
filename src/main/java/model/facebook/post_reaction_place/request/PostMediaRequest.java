9dc7c3f85a28f066072715e9eb6692aed7a02ef8
Kamran <vkamran7@gmail.com>
2021-02-21 19:19:41 +0400

    added PostMediaRequest dto

---
+++ src/main/java/model/facebook/post_reaction_place/request/PostMediaRequest.java
@@ -0,0 +1,52 @@
+package model.facebook.post_reaction_place.request;
+
+public class PostMediaRequest {
+    private String query; // Post/Photo/Video ID
+    private Long ownerID; // Owner of media
+    private Long wallOwner;
+    private Integer limit;
+
+
+    public static final class Builder {
+        private String query; // Post/Photo/Video ID
+        private Long ownerID; // Owner of media
+        private Long wallOwner;
+        private Integer limit;
+
+        public Builder() {
+        }
+
+        public static Builder aPostMediaRequest() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder withOwnerID(Long ownerID) {
+            this.ownerID = ownerID;
+            return this;
+        }
+
+        public Builder withWallOwner(Long wallOwner) {
+            this.wallOwner = wallOwner;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public PostMediaRequest build() {
+            PostMediaRequest postMediaRequest = new PostMediaRequest();
+            postMediaRequest.wallOwner = this.wallOwner;
+            postMediaRequest.query = this.query;
+            postMediaRequest.ownerID = this.ownerID;
+            postMediaRequest.limit = this.limit;
+            return postMediaRequest;
+        }
+    }
+}
