5f45ca284e1a87445d6835884085f9db1853fa45
Kamran <vkamran7@gmail.com>
2021-02-21 15:41:09 +0400

    added AlbumPhotosRequest dto to page package

---
+++ src/main/java/model/facebook/page/request/AlbumPhotosRequest.java
@@ -0,0 +1,44 @@
+package model.facebook.page.request;
+
+public class AlbumPhotosRequest {
+    private String query;
+    private Integer limit;
+    private Integer timeout;
+
+
+    public static final class Builder {
+        private String query;
+        private Integer limit;
+        private Integer timeout;
+
+        public Builder() {
+        }
+
+        public static Builder anAlbumPhotosRequest() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public Builder withTimeout(Integer timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public AlbumPhotosRequest build() {
+            AlbumPhotosRequest albumPhotosRequest = new AlbumPhotosRequest();
+            albumPhotosRequest.timeout = this.timeout;
+            albumPhotosRequest.limit = this.limit;
+            albumPhotosRequest.query = this.query;
+            return albumPhotosRequest;
+        }
+    }
+}
