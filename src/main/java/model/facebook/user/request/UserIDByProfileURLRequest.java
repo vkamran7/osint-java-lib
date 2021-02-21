cb4d691088b9d1c5ae2108575e9b8fa0400e2cfa
Kamran <vkamran7@gmail.com>
2021-02-21 15:20:39 +0400

    added UserIDByProfileURLRequest dto to user package

---
+++ src/main/java/model/facebook/user/request/UserIDByProfileURLRequest.java
@@ -0,0 +1,28 @@
+package model.facebook.user.request;
+
+public class UserIDByProfileURLRequest {
+    private String url;  // Facebook Profile URL
+
+
+    public static final class Builder {
+        private String url;  // Facebook Profile URL
+
+        public Builder() {
+        }
+
+        public static Builder anUserIDByProfileURLRequest() {
+            return new Builder();
+        }
+
+        public Builder withUrl(String url) {
+            this.url = url;
+            return this;
+        }
+
+        public UserIDByProfileURLRequest build() {
+            UserIDByProfileURLRequest userIDByProfileURLRequest = new UserIDByProfileURLRequest();
+            userIDByProfileURLRequest.url = this.url;
+            return userIDByProfileURLRequest;
+        }
+    }
+}
