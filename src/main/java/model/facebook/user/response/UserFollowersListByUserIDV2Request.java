8b827a80470bf2dbc688700ec6634d1c0e073c50
Kamran <vkamran7@gmail.com>
2021-02-21 14:51:14 +0400

    added UserFollowersListByUserIDV2Request dto class to user package

---
+++ src/main/java/model/facebook/user/response/UserFollowersListByUserIDV2Request.java
@@ -0,0 +1,44 @@
+package model.facebook.user.response;
+
+public class UserFollowersListByUserIDV2Request {
+    private String query;
+    private Integer limit;
+    private Integer timeout;
+
+
+    public static final class Builder {
+        private String query;
+        private Integer limit;
+        private Integer timeout;
+
+        public Builder() {
+        }
+
+        public static Builder anUserFollowersListByUserIDV2Request() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public Builder withTimeout(Integer timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public UserFollowersListByUserIDV2Request build() {
+            UserFollowersListByUserIDV2Request userFollowersListByUserIDV2Request = new UserFollowersListByUserIDV2Request();
+            userFollowersListByUserIDV2Request.timeout = this.timeout;
+            userFollowersListByUserIDV2Request.limit = this.limit;
+            userFollowersListByUserIDV2Request.query = this.query;
+            return userFollowersListByUserIDV2Request;
+        }
+    }
+}
