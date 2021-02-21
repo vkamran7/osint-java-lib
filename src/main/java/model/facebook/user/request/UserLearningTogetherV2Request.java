eeba5de0fe54b4ef7edef6c760dea03ee29fbec1
Kamran <vkamran7@gmail.com>
2021-02-21 15:23:54 +0400

    added UserLearningTogetherV2Request dto to user package

---
+++ src/main/java/model/facebook/user/request/UserLearningTogetherV2Request.java
@@ -0,0 +1,68 @@
+package model.facebook.user.request;
+
+public class UserLearningTogetherV2Request {
+    private String query;  // Facebook User ID
+    private Integer limit;
+    private Integer timeout;
+    private Integer delayed;
+    private Integer taskTimeout;
+    private Integer taskId;
+
+
+    public static final class Builder {
+        private String query;  // Facebook User ID
+        private Integer limit;
+        private Integer timeout;
+        private Integer delayed;
+        private Integer taskTimeout;
+        private Integer taskId;
+
+        public Builder() {
+        }
+
+        public static Builder anUserLearningTogetherV2Request() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public Builder withTimeout(Integer timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public Builder withDelayed(Integer delayed) {
+            this.delayed = delayed;
+            return this;
+        }
+
+        public Builder withTaskTimeout(Integer taskTimeout) {
+            this.taskTimeout = taskTimeout;
+            return this;
+        }
+
+        public Builder withTaskId(Integer taskId) {
+            this.taskId = taskId;
+            return this;
+        }
+
+        public UserLearningTogetherV2Request build() {
+            UserLearningTogetherV2Request userLearningTogetherV2Request = new UserLearningTogetherV2Request();
+            userLearningTogetherV2Request.taskId = this.taskId;
+            userLearningTogetherV2Request.delayed = this.delayed;
+            userLearningTogetherV2Request.taskTimeout = this.taskTimeout;
+            userLearningTogetherV2Request.timeout = this.timeout;
+            userLearningTogetherV2Request.limit = this.limit;
+            userLearningTogetherV2Request.query = this.query;
+            return userLearningTogetherV2Request;
+        }
+    }
+}
