af025b031fadd11e3e6170a49464a5bd78caea2c
Kamran <vkamran7@gmail.com>
2021-02-21 16:37:25 +0400

    added PhotosByPageRequest dto

---
+++ src/main/java/model/facebook/page/request/PhotosByPageRequest.java
@@ -0,0 +1,36 @@
+package model.facebook.page.request;
+
+public class PhotosByPageRequest {
+    private String query; // Facebook Page ID
+    private Integer limit;
+
+
+    public static final class Builder {
+        private String query;
+        private Integer limit;
+
+        public Builder() {
+        }
+
+        public static Builder aPhotosByPageRequest() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public PhotosByPageRequest build() {
+            PhotosByPageRequest photosByPageRequest = new PhotosByPageRequest();
+            photosByPageRequest.query = this.query;
+            photosByPageRequest.limit = this.limit;
+            return photosByPageRequest;
+        }
+    }
+}
