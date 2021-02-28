ea8e3db6eca384ac72a637b067677bf3e186f80d
Kamran <vkamran7@gmail.com>
2021-02-28 16:38:46 +0400

    added GetPlacesGraphSearchRequest dto

---
+++ src/main/java/model/facebook/graph/request/GetPlacesGraphSearchRequest.java
@@ -0,0 +1,56 @@
+package model.facebook.graph.request;
+
+public class GetPlacesGraphSearchRequest {
+    private String query;
+    private Integer limit;
+    private Integer timeout;
+
+    public String getQuery() {
+        return query;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public Integer getTimeout() {
+        return timeout;
+    }
+
+
+    public static final class Builder {
+        private String query;
+        private Integer limit;
+        private Integer timeout;
+
+        public Builder() {
+        }
+
+        public static Builder aGetPlacesGraphSearchRequest() {
+            return new Builder();
+        }
+
+        public Builder withQuery(String query) {
+            this.query = query;
+            return this;
+        }
+
+        public Builder withLimit(Integer limit) {
+            this.limit = limit;
+            return this;
+        }
+
+        public Builder withTimeout(Integer timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public GetPlacesGraphSearchRequest build() {
+            GetPlacesGraphSearchRequest getPlacesGraphSearchRequest = new GetPlacesGraphSearchRequest();
+            getPlacesGraphSearchRequest.limit = this.limit;
+            getPlacesGraphSearchRequest.timeout = this.timeout;
+            getPlacesGraphSearchRequest.query = this.query;
+            return getPlacesGraphSearchRequest;
+        }
+    }
+}
